897dddd6ad832f792ef0495cc9958b8668ef4bd7
Thiago Franco de Moraes <totonixsame@gmail.com>
2013-05-17 15:29:35 -0300

    Added comments on generate_kml_by_file.py

--- Python/generate_kml_by_file.py
+++ Python/generate_kml_by_file.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# This script take as parameter a file with a place by line. A search for each
+# place is done using pygeocoder[1]. To each place it generates a markers. The set
+# of marker are then saved in kml file using simplekml. This script prints on
+# the screen all the places pygeocoder were not able to find.
+# [1] - http://code.xster.net/pygeocoder/wiki/Home
+# [2] - http://code.google.com/p/simplekml/
 import sys
 import pygeocoder
 import simplekml
@@ -5,7 +13,6 @@ import simplekml
 from pygeocoder import Geocoder
 
 with open(sys.argv[1]) as f:
-    non_ok = []
     kml = simplekml.Kml()
     for h in f:
         try:
